1152f456dbda15916c607fa417a6e1621ef66140
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-23 10:08:58 -0700

    Modified the header and footer folder structure for simplicity and added a fragment to display the header with the homepage render.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { Contact } from "./pages/Contact";
 import { Project } from "./pages/Project";
 import { Resume } from "./pages/Resume";
 
+import Header from "./components/ui/Header"
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+    <>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/Contact' element={<Contact />} />
       <Route path='/Project' element={<Project />} />
       <Route path='/Resume' element={<Resume />} />
     </Routes>
+    </>
   );
 }
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -4,11 +4,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import logo from '../../../assets/images/MainLogo.png';
+import logo from '../../MainLogo.png';
 
 const Header = () => {
     return (
-        <Navbar fixed='top' collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">
             <img 
