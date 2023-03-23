784730f5afa5348697eeb129ec45bb254ab4b2ae
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-23 09:43:06 -0700

    Made adjustments to index rendering and added React Routing.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+const root = document.getElementById('root')
+render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  root
+)
+
