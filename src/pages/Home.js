79dcda9b5c78d88ee47ab1f156704cab64c19b16
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-25 10:28:30 -0700

    Decided on background style and need to fill out the home page design.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,24 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
 export const Home = () => {
-    return <h1>Home page</h1>
+    const styles = {
+        background: {
+            backgroundColor: '#212529',
+            height: '100vh'
+        },
+        text: {
+            color: 'white',
+            textAlign: 'center',
+        }
+    }
+    return (
+        <div style={styles.background}>
+            <Container>
+                <div style={styles.text}>
+                    
+                </div>
+            </Container>
+        </div>
+    )
 }
