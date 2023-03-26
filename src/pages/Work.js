58a6fd8dee190466ad9528def525f1b806f46e34
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-26 11:13:27 -0700

    Added second card with styling. Thinking of testing a carousel.

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,5 +1,6 @@
 import { Container, Card } from "react-bootstrap";
-import meetIO from "../images/MeetIOProject.png"
+import meetIO from "../images/MeetIOProject.png";
+import yodaTranslate from "../images/YodaProject.png";
 
 export const Work = () => {
     const styles = {
@@ -10,16 +11,38 @@ export const Work = () => {
         text: {
             color: 'white',
             textAlign: 'center',
+        },
+        card: {
+            width: '50%',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            paddingTop: '5%'
+        },
+        img: {
+            height: '270px',
+            width: '100%',
         }
     }
     return (
         <div style={styles.background}>
             <Container>
-                <div style={styles.text}>
+                <div className="meetIO-card" style={{...styles.text, ...styles.card}}>
                     <Card className="bg-dark text-white">
-                        <Card.Img src={meetIO} alt="Card image" />
+                        <Card.Img src={meetIO} style={styles.img} alt="Card image" />
                         <Card.ImgOverlay>
                             <Card.Title>Meet-IO</Card.Title>
+                            <Card.Text>
+                                Sample Text
+                            </Card.Text>
+                            <Card.Text>Secondary Text</Card.Text>
+                        </Card.ImgOverlay>
+                    </Card>
+                </div>
+                <div className="yodaTranslate-card" style={{...styles.text, ...styles.card}}>
+                    <Card className="bg-dark text-white">
+                        <Card.Img src={yodaTranslate} style={styles.img} alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Yoda Translate</Card.Title>
                             <Card.Text>
                                 Sample Text
                             </Card.Text>
