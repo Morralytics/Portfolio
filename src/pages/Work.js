eb5323e515baa5123e3af4dffab51559f1b036f4
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-26 10:51:53 -0700

    Added new card with an image and text overlay. Need to edit the descriptions.

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,4 +1,5 @@
-import { Container } from "react-bootstrap"
+import { Container, Card } from "react-bootstrap";
+import meetIO from "../images/MeetIOProject.png"
 
 export const Work = () => {
     const styles = {
@@ -15,7 +16,16 @@ export const Work = () => {
         <div style={styles.background}>
             <Container>
                 <div style={styles.text}>
-                    
+                    <Card className="bg-dark text-white">
+                        <Card.Img src={meetIO} alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Meet-IO</Card.Title>
+                            <Card.Text>
+                                Sample Text
+                            </Card.Text>
+                            <Card.Text>Secondary Text</Card.Text>
+                        </Card.ImgOverlay>
+                    </Card>
                 </div>
             </Container>
         </div>
