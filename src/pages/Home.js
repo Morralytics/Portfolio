da3165a7100d7a3001bda10f8e9b0bc13ed88fd1
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-26 21:59:30 -0700

    Added profile picture and basic structure of the layout.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import ProfilePicure from "../images/ProfilePicture.jpg";
 
 export const Home = () => {
     const styles = {
@@ -7,16 +8,46 @@ export const Home = () => {
             backgroundColor: '#212529',
             height: '100vh'
         },
-        text: {
-            color: 'white',
+        textAlign: {
             textAlign: 'center',
+        },
+        textStyle: {
+            color: 'white',
+            fontFamily: 'Open Sans',
+        },
+        textHeader: {
+            fontSize: "50px",
+        },
+        textContent: {
+            fontSize: "25px",
+        },
+        imgAlign: {
+            width: "40%",
+            marginLeft: "auto",
+            marginRight: "auto",
         }
     }
     return (
         <div style={styles.background}>
             <Container>
-                <div style={styles.text}>
-                    
+                <div style={{...styles.textAlign, ...styles.textStyle, ...styles.textHeader}}>
+                    <p>
+                        Hello and welcome!
+                    </p>
+                </div>
+                <div style={styles.imgAlign}>
+                    <img
+                        className="d-block w-100 img-fluid rounded-circle"
+                        src={ProfilePicure}
+                        alt="Profile"
+                    />
+                </div>
+                <div style={{...styles.textAlign, ...styles.textStyle, ...styles.textContent}}>
+                    <p>
+                        My name is Nick Morris
+                        <br></br>
+                        I am a Full Stack Engineer based in beautiful Bend Oregon where I design and build websites for everyone to use! 
+                    </p>
                 </div>
             </Container>
         </div>
