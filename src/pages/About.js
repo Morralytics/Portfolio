3dfc1617c1b9b4131ed957e3f9a1b6e3041afd92
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-26 22:18:27 -0700

    Added basic structure of cards for the about me page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap"
+import { Container, Card, CardGroup } from "react-bootstrap"
 
 export const About = () => {
     const styles = {
@@ -7,7 +7,7 @@ export const About = () => {
             height: '100vh'
         },
         text: {
-            color: 'white',
+            color: 'black',
             textAlign: 'center',
         }
     }
@@ -15,7 +15,44 @@ export const About = () => {
         <div style={styles.background}>
             <Container>
                 <div style={styles.text}>
-                    
+                    <CardGroup>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Sample Title</Card.Title>
+                                <Card.Text>
+                                    Text content
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Supporting text</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Sample Title</Card.Title>
+                                <Card.Text>
+                                    Text content
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Supporting text</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Sample Title</Card.Title>
+                                <Card.Text>
+                                    Text content
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Supporting text</small>
+                            </Card.Footer>
+                        </Card>
+                    </CardGroup>
                 </div>
             </Container>
         </div>
