b4cac2ee6b190e9cdbd7775039b16404669dc64d
Nick Morris <nicholasmorris2599@gmail.com>
2023-05-16 08:58:10 -0700

    Apollo server staged. Still need to configure the connection to the database and path routing.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
+// Still need to setup connection file for database connection
+const db = require('./config/connection');
+const { type } = require('os');
 
 const PORT = process.env.PORT | 3001;
 const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Need to set up apollo server
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`The API server is running on port ${PORT}.`);
+            console.log(`GraphQL is running at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
+
+startApolloServer(typeDefs, resolvers);
