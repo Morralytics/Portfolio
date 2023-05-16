bbfa0a0ef7391ff77f10cb021ea968dc2faf558b
Nick Morris <nicholasmorris2599@gmail.com>
2023-05-16 12:54:27 -0700

    Added resolver which currently only needs to query all projects.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const { Project } = require('../models');
+
+const resolvers = {
+    Query: {
+        projects: async () => {
+            return Project.find()
+        },
+    },
+};
+
+module.exports = resolvers;
