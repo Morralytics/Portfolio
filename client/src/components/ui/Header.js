8b6c2ffb0154a71b4654acb2a871f79d862f50a7
Nick Morris <nicholasmorris2599@gmail.com>
2023-05-21 15:25:54 -0700

    Bug fix for improper showing of dropdown button while in mobile.

--- client/src/components/ui/Header.js
+++ client/src/components/ui/Header.js
@@ -3,6 +3,8 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -16,6 +18,9 @@ const Header = () => {
     bgDarkCustom: {
       backgroundColor: '#212529',
       boxShadow: 'none'
+    },
+    custom: {
+      backgroundImage: 'url("data:image/svg+xml,%3csvg xmlns=%27http://www.w3.org/2000/svg%27 viewBox=%270 0 30 30%27%3e%3cpath stroke=%27rgba%28255, 255, 255, 0.55%29%27 stroke-linecap=%27round%27 stroke-miterlimit=%2710%27 stroke-width=%272%27 d=%27M4 7h22M4 15h22M4 23h22%27/%3e%3c/svg%3e")'
     }
   }
   return (
@@ -30,7 +35,9 @@ const Header = () => {
             alt='Brand Logo'
           />
         </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle type='button' aria-label='Toggle navigation' className='navbar-toggler collapsed'>
+          <span className='navbar-toggler-icon' style={styles.custom}></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Link to='/About' className='nav-link'>About me</Link>
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const container = document.getElementById('root')
