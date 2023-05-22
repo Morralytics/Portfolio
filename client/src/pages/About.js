11bbe9794e9da5b20b11425cce905b8e27d7e2d2
Nick Morris <nicholasmorris2599@gmail.com>
2023-05-21 17:07:33 -0700

    Adjusted the about me page to fit mobile versions.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -11,24 +11,6 @@ export const About = () => {
             backgroundColor: '#212529',
             height: '100vh'
         },
-        textHeader: {
-            color: 'white',
-            textAlign: 'center',
-            fontFamily: 'Open Sans',
-            fontSize: '20px',
-            width: '50%',
-            marginLeft: "auto",
-            marginRight: "auto",
-        },
-        secondaryText: {
-            color: 'white',
-            textAlign: 'center',
-            fontFamily: 'Open Sans',
-            fontSize: '20px',
-            width: '70%',
-            marginLeft: "auto",
-            marginRight: "auto",
-        },
         listStyle: {
             lineHeight: '30px',
             listStyleType: 'none',
@@ -42,7 +24,7 @@ export const About = () => {
     return (
         <div style={styles.background}>
             <Container style={styles.background}>
-                <div style={styles.textHeader}>
+                <div className="photographyText" style={styles.textHeader}>
                     <p>
                         Photography is a hobby of mine where I can fully express an eye for design and placement of objects. Similar to constructing a website, certain characteristics can be the highlight while supporting elements create the final piece.
                     </p>
@@ -52,17 +34,10 @@ export const About = () => {
                     photographs={photographs}
                 />
                 <br></br>
-                <div style={styles.secondaryText}>
+                <div className="photographyBackupText" style={styles.secondaryText}>
                     <p>
                         Each role of a Software Engineer is incredibly import for the stability of the application being worked on. The skills that need to be brought to the table now is more than just a simple webpage and the skills I have are as follows:
                     </p>
-                    {/* <ul style={styles.listStyle}>
-                        <li>React.js</li>
-                        <li>Packages through the Node Package Manager</li>
-                        <li>Relational and Non-Relational Databases like MySQL and MongoDB</li>
-                        <li>A background in Computer Science and the Python language</li>
-                        <li>Websocket interactions like Socket.io</li>
-                    </ul> */}
                 </div>
             </Container>
         </div>
